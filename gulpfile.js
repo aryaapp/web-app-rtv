8522b6fdfa5e6410b24a2d33eaf49cbf168b6a30
DavidGentham <groetenuitparamaribo@hotmail.com>
2015-10-26 17:56:11 +0100

    hello world set up react, gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+// INSTRUCTIONS: http://ericlbarnes.com/setting-gulp-bower-bootstrap-sass-fontawesome/
+
+// defining dependencies and object to refer to paths
+var 	gulp = require('gulp'),
+		browserify = require('gulp-browserify'),
+		reactify = require('reactify'),
+		transform = require('vinyl-transform'),
+		jshint = require('gulp-jshint'),
+		babel = require("gulp-babel"),
+		less = require('gulp-less'),
+		path = require('path'),
+		connect = require('gulp-connect'),
+		config = {
+			lessDir: '../arya-website/less',
+			jsDir: './src/javascript',
+			staticDir: './src/static'
+		};
+
+// moves fonts to /public folder
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+		.pipe(gulp.dest('./dist/fonts'));
+});
+
+// moves static folders to /public folder
+gulp.task('static', function() {
+    return gulp.src(config.staticDir + '/**/*.*')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('less', function () {
+  return gulp.src(config.lessDir + '/**/styles_dash.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'config.lessDir', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dist/css'))
+});
+
+//takes app.js,  runs converts javasript ES6, minifies, then builds to /dist folder
+gulp.task('jshint', function () {
+  return gulp.src(config.jsDir + '**/*.js')
+	.pipe(jshint({ esnext : true })) // lets jshint know we're using es6
+	.pipe(jshint.reporter('default'))
+});
+
+gulp.task('babel', function () {
+	return gulp.src(config.jsDir + '/**/*.js')
+    .pipe(babel()) //convert ES6 to ES5
+    .pipe(gulp.dest('./src/js5'))
+});
+
+//bundle is dependant on babel to run first
+gulp.task('bundle', ['babel'], function () {
+	return gulp.src('./src/js5/app.js')
+    .pipe(browserify(
+    	{ transform: [reactify] }
+    ))
+    .pipe(gulp.dest('./dist/js'))
+});
+
+// gulp.task('bundle',function() {
+// 	return gulp.src('./src/js5/app.js')
+//     .pipe(browserify({ // combine nodes to single .js file
+//       transform: [reactify]
+//     })
+//     .pipe(gulp.dest('./public/js')) // save in public folder
+// });
+
+// makes it easy for collaborators to install everything by running gulp-bower
+gulp.task('bower', function() {
+    return bower()
+		.pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+});
+gulp.task('html', function () {
+  gulp.src('./dist/index.html')
+    .pipe(connect.reload());
+});
+
+// gulp.task('doc', function () {
+//   gulp.src(config.jsDir + '/**/*.js')
+//   	.pipe(yuidoc())
+//   	.pipe(gulp.dest("./dist/doc"));
+// });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+	gulp.watch(config.lessDir + '/**/*.less', ['less']);
+	gulp.watch(config.jsDir + '/**/*.js', ['babel','bundle']);
+	gulp.watch(config.staticDir + '/**/*.*', ['static']);
+});
+// runs basic tasks when first using gulp
+gulp.task('default', ['bower', 'icons', 'css']);
---
+++ src/javascript/app.js
@@ -0,0 +1,29 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('react-app')
+);
+
+
+// $(document).ready(function() {
+//     $('#fullpage').fullpage({
+//         sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+//         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+//         menu: '#menu',
+//         css3: true,
+//         scrollingSpeed: 1000
+//     });
+
+//     $('#showExamples').click(function(e){
+//         e.stopPropagation();
+//         e.preventDefault();
+//         $('#examplesList').toggle();
+//     });
+
+//     $('html').click(function(){
+//         $('#examplesList').hide();
+//     });
+// });
