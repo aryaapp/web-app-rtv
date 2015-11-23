04bcca62e1d51c1b063a71c7b2e993252ff4861e
DavidGentham <groetenuitparamaribo@hotmail.com>
2015-11-23 12:06:36 +0100

    vector images for smileys, not aligned properly yet

--- src/javascript/components/Question/SliderInput.react.js
+++ src/javascript/components/Question/SliderInput.react.js
@@ -19,7 +19,28 @@ let SliderInput = React.createClass({
   },
   render: function() {
     return (
-      <ReactSlider defaultValue={ 90 } />
+      <div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-push-1">
+            <ReactSlider defaultValue={ 90 } />
+          </div>
+        </div>
+          <div className="col-xs-2 col-xs-push-1 no-padding">
+            <img src="images/emo-1.svg" className="svg emo emo-1" />
+          </div>
+          <div className="col-xs-2 col-xs-push-1 no-padding">
+            <img src="images/emo-2.svg" className="svg emo emo-2" />
+          </div>
+          <div className="col-xs-2 col-xs-push-1 no-padding">
+            <img src="images/emo-3.svg" className="svg emo emo-3" />
+          </div>
+          <div className="col-xs-2 col-xs-push-1 no-padding">
+            <img src="images/emo-4.svg" className="svg emo emo-4" />
+          </div>
+          <div className="col-xs-2 col-xs-push-1 no-padding">
+            <img src="images/emo-5.svg" className="svg emo emo-5" />
+          </div>
+        </div>
     )
   }
 })
