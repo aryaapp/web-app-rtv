431a918da184de6c5715fa6317fc868c1616038d
DavidGentham <groetenuitparamaribo@hotmail.com>
2015-11-27 18:25:30 +0100

    styling, included rtv logo banner, animation easing

--- src/javascript/fullpage.js
+++ src/javascript/fullpage.js
@@ -9,12 +9,19 @@ $(document).ready(function() {
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
         menu: '#menu',
         css3: true,
+        easingcss3: 'ease-out',
         navigation: true,
         navigationPosition: 'right',
-        scrollingSpeed: 1000,
-        scrollOverflow: true
+        scrollingSpeed: 800,
+        scrollOverflow: true,
+        normalScrollElements: ""
     });
     $.fn.fullpage.setAllowScrolling(false);
     $.fn.fullpage.setKeyboardScrolling(false);
     $('#fp-nav').hide();
+
+    $('input, select, textarea').on('focus blur', function(event) {
+        $('meta[name=viewport]').attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+    });
+
 });
