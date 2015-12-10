da3c4b6824b71595515b86b36cb3a7563f402a19
DavidGentham <groetenuitparamaribo@hotmail.com>
2015-12-10 17:16:16 +0100

    styling

--- src/javascript/components/Question/WelcomeModal.react.js
+++ src/javascript/components/Question/WelcomeModal.react.js
@@ -44,8 +44,8 @@ var WelcomeModal = React.createClass({
           <div className="welcome-modal container-fluid">
             <div className="row full-height">
             <div className="col-xs-12 no-padding full-height">
-            <img src="./images/rtv_logo-1.png" className="welcome-rtv-image fade-in arya-animation animation2"/>
-            <div className="col-xs-12 tagebuch fade-in arya-animation animation2">Achtsamkeits-Tagebuch</div>
+            <img src="./images/rtv_tagebuch_logo-1.png" className="welcome-rtv-image fade-in arya-animation animation2"/>
+            
             <div className="welcome-text-container rtv-title col-xs-12 vertical-align-center fade-in arya-animation animation1">
               
               <h3>
