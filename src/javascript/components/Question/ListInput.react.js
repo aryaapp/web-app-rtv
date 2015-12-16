a06579d527fbea871f679b66d5d0a3986dfd3fee
DavidGentham <groetenuitparamaribo@hotmail.com>
2015-12-16 14:00:44 +0100

    added onFocus and onBlur callbacks to listInput.react, couldn't finish. callbacks are working, hiding needs to be added

--- src/javascript/components/Question/ListInput.react.js
+++ src/javascript/components/Question/ListInput.react.js
@@ -4,6 +4,22 @@
 */
 import React from 'react';
 
+let detectMobile = function () { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
 var ListInput = React.createClass({
   getDefaultProps() {
     return {
@@ -45,12 +61,26 @@ var ListInput = React.createClass({
     this.update(newParentState)
 
   },
+  handleFocus: function() {
+     if(detectMobile()) {
+      console.log('focus')
+      //make nav buttons invisible
+     }
+  },
+  handleBlur: function() {
+    if(detectMobile()) {
+     console.log('blur')
+     //make nav buttons visible
+   }
+  },
   render: function() {
     var that = this
     return (
       <div>
         <form onSubmit={this.addValue}>
           <input
+            onFocus={this.handleFocus} 
+            onBlur={this.handleBlur}
             className="form-control"
             type='text'
             value={this.state.newValue}
