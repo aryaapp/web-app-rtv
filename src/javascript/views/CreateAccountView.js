5ac06ac7270ee8cdf7e97b401fa4432e3f9077e6
Martin Frank <martin.frank.ts@gmail.com>
2016-01-27 12:02:41 +0100

    extracting the create account form

--- src/javascript/views/CreateAccountView.js
+++ src/javascript/views/CreateAccountView.js
@@ -3,128 +3,19 @@
 * @submodule Question
 */
 import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { routeActions } from 'react-router-redux'
-import { reduxForm } from 'redux-form'
-import Recaptcha from 'react-google-recaptcha'
 
 import { executeCreateAccount } from '../actions/createAccount'
-import { executeSaveJournal, unscheduleJournalSave } from '../actions/journals'
-import {
-  defaultQuestionnaireId,
-  feelingQuestionId,
-  bodyQuestionId,
-  thoughtsQuestionId,
-  situationQuestionId,
-  reactionQuestionId
-} from '../constants/ids'
-import { intersperse, reverseArray } from '../utilities'
-
-import NextButton from '../components/Reusable/NextButton.react.js'
-import PrevButton from '../components/Reusable/PrevButton.react.js'
 
 import QuestionTitle from '../components/Question/QuestionTitle.react.js'
 import QuestionSubtitle from '../components/Question/QuestionSubtitle.react.js'
 import QuestionHeader from '../components/Question/QuestionHeader.react.js'
 import QuestionMain from '../components/Question/QuestionMain.react.js'
-
-const createAccountFormValidation = (values) => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  if (!values.password) {
-    errors.password = 'Required'
-  } else if (values.password.length < 8) {
-    errors.password = 'At 8 characters'
-  }
-
-  if (!values.password_confirmation) {
-    errors.password_confirmation = 'Required'
-  } else if (values.password !== values.password_confirmation) {
-    errors.password_confirmation = "Doesn't match password."
-  }
-
-  if (!values.recaptcha) {
-
-  }
-  return errors;
-}
-
-class CreateAccountForm extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { fields: { email, password, password_confirmation, recaptcha}, handleSubmit } = this.props;
-
-    let emailInvalidLabel = <label className="validation-message">Bitte gib eine gültige E-Mailadresse ein.</label>
-    let passwordInvalidLabel = <label className="validation-message">Bitte gib eine Passwort mit mindestes 8 Zeichen ein.</label>
-    let passwordConfirmationInvalidLabel = <label className="validation-message">Bitte gib das Password ein zweites mal genau gleich ein.</label>
-    let captachNotConfirmed = <label className="validation-message">Bitte bestätige, dass du auch wirklich ein Mensch bist ;-)</label>
-
-    return(
-      <div className="form-group">
-        <div className="col-xs-12">
-
-        </div>
-        <form onSubmit={ handleSubmit } >
-            <input
-              className="form-control email-control"
-              type='text'
-              placeholder="Ihre E-Mailadresse"
-              aria-describedby="basic-addon1"
-              {...email} />
-            { email.touched && email.error ? emailInvalidLabel : '' }
-            <input
-              className="form-control email-control"
-              type="password"
-              placeholder="Ihre Passwort"
-              {...password} />
-            { password.touched && password.error ? passwordInvalidLabel : '' }
-            <input
-              className="form-control email-control"
-              type="password"
-              placeholder="Nochmal das Passwort"
-              {...password_confirmation} />
-              { password_confirmation.touched && password_confirmation.error ? passwordConfirmationInvalidLabel : '' }
-            <div className="col-xs-12 margin-top">
-              <p>Bitte bestätige, dass du auch wirklich ein Mensch bist ;-)</p>
-              <Recaptcha
-                ref="recaptcha"
-                sitekey="6LdJ2RETAAAAAPHK7GmcRZTPnZY0E3AGY0sivpAs"
-                {...recaptcha}
-              />
-            </div>
-            <button className="btn btn-primary nav-button next-button relative-button" onClick={ handleSubmit } >
-              Zugang erstellen
-            </button>
-        </form>
-      </div>
-    )
-  }
-}
-
-const ReduxCreateAccountForm = reduxForm({
-  form: 'createAccount',
-  fields: ['email', 'password', 'password_confirmation', 'recaptcha'],
-  validate: createAccountFormValidation
-})(CreateAccountForm);
-
-
-const mapStateToProps = (state) => (state)
+import CreateAccountForm from '../components/CreateAccountForm'
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    executeCreateAccount: (email, password) => dispatch(executeCreateAccount(email, password)),
-    executeSaveJournal: (journal_data) => dispatch(executeSaveJournal(journal_data)),
-    unscheduleJournalSave: () => dispatch(unscheduleJournalSave()),
-    navLogin: () => dispatch(routeActions.push('/login'))
+    executeCreateAccount: (email, password) => dispatch(executeCreateAccount(email, password))
   }
 }
 
@@ -149,7 +40,7 @@ class CreateAccountView extends Component {
             <QuestionSubtitle subtitle="Erstelle einen Zugang, um deine Einträge in deinem persönlichen Achtsamkeits-Tagebuch zu speichern"/>
           </QuestionHeader>
           <QuestionMain>
-            <ReduxCreateAccountForm
+            <CreateAccountForm
               onSubmit={this.submitCreateAccount}
             />
           </QuestionMain>
@@ -164,8 +55,4 @@ CreateAccountView.propTypes = {
   executeCreateAccount: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateAccountView)
-
-// <button className="test-btn" onClick={ this.props.navLogin } >
-//             <span className="btn-text">Zum Einloggen</span>
-//           </button>
+export default connect(null, mapDispatchToProps)(CreateAccountView)
