e46f6e7ec73a79dcec8202e8fa82d860138470db
Martin Frank <martin.frank.ts@gmail.com>
2016-02-09 14:24:56 +0100

    Customize Welcome Message for loggedin users
    https://trello.com/c/nlTGpDfT
    
    Show different message depending on time of day and number of journals. Automatic redirect after 10 seconds.

--- src/javascript/views/WelcomeView.js
+++ src/javascript/views/WelcomeView.js
@@ -8,6 +8,7 @@ import { routeActions } from 'react-router-redux'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import FixedSectionFooter from '../components/Question/FixedSectionFooter.react.js'
+import { size } from 'lodash'
 
 import Modal from 'react-modal'
 import Content from '../constants/localizableStringsDE.js'
@@ -26,17 +27,21 @@ class WelcomeView extends Component {
   constructor(props) {
     super(props)
 
+    this.state = { loggedIn: false }
+
     this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
+    this.componentWillMount = this.componentWillMount.bind(this)
     this.checkForRedirect = this.checkForRedirect.bind(this)
   }
 
   checkForRedirect(props) {
     if (typeof props.access_token !== 'undefined' && props.access_token.length > 0) {
-      props.navHome();
+      console.log('checkForRedirect token present')
+      this.setState({ loggedIn: true })
+      setTimeout(props.navHome, 10000)
     }
   }
-  componentDidMount() {
+  componentWillMount() {
     this.checkForRedirect(this.props)
   }
 
@@ -45,13 +50,59 @@ class WelcomeView extends Component {
   }
 
   displayWelcomeMessage() {
-    return Content.INTRO_MESSAGES[Math.floor(Math.random() * Content.INTRO_MESSAGES.length)]
+    if(this.state.loggedIn) {
+      const journalCount = size(this.props.journals)
+      switch(journalCount) {
+        case 2:
+          return Content.SMART_INTRO_MESSAGES.two
+        case 4:
+          return Content.SMART_INTRO_MESSAGES.four
+        case 6:
+          return Content.SMART_INTRO_MESSAGES.six
+        case 10:
+          return Content.SMART_INTRO_MESSAGES.ten
+      }
+      const now = new Date()
+      let messagePool = []
+
+      if(now.getHours() < 12) {
+        messagePool = messagePool.concat(
+          Content.SMART_INTRO_MESSAGES.morning,
+          Content.SMART_INTRO_MESSAGES.morning
+        )
+      }
+
+      if(now.getHours() > 18) {
+        messagePool = messagePool.concat(
+          Content.SMART_INTRO_MESSAGES.evening,
+          Content.SMART_INTRO_MESSAGES.evening
+        )
+      }
+
+      messagePool = messagePool.concat(Content.SMART_INTRO_MESSAGES.random)
+      let message = messagePool[Math.floor(Math.random() * messagePool.length)]
+      return message
+    } else {
+      return Content.INTRO_MESSAGES[Math.floor(Math.random() * Content.INTRO_MESSAGES.length)]
+    }
   }
 
   render() {
-    let date = new Date();
-    let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-
+    const date = new Date();
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    const loginButtons = <div className="col-xs-12 buttons-container">
+                      <button className="btn btn-primary nav-button next-button relative-button fade-in arya-animation animation3" onClick={this.props.navFeeling}>
+                        <span className="btn-text">Ich bin neu hier</span>
+                      </button>
+                      <button className="btn nav-button next-button relative-button fade-in arya-animation animation3" onClick={this.props.navLogin}>
+                        <span className="btn-text">Anmelden </span>
+                      </button>
+                    </div>
+    const forwardButton = <div className="col-xs-12 buttons-container">
+                            <button className="btn btn-primary nav-button next-button relative-button fade-in arya-animation animation3" onClick={this.props.navHome}>
+                              <span className="btn-text">Weiter</span>
+                            </button>
+                          </div>
     return (
       <div className="partial-wrapper">
         <div className="partial-container" >
@@ -64,19 +115,13 @@ class WelcomeView extends Component {
                   <p className="col-xs-12 heute fade-in arya-animation animation2">Heute, hier und jetzt.<br/>{ date.toLocaleDateString('de-DE', options) }</p>
                 </div>
                 <div className="col-xs-12 welcome-text margin-bottom fade-in arya-animation animation1">
-                  <i className="">{ this.displayWelcomeMessage() }</i>
+                  <i>{ this.displayWelcomeMessage() }</i>
                 </div>
               </div>
             </div>
+            { !this.state.loggedIn ? loginButtons : forwardButton }
           </div>
-          <div className="col-xs-12 buttons-container">
-            <button className="btn btn-primary nav-button next-button relative-button fade-in arya-animation animation3" onClick={this.props.navFeeling}>
-              <span className="btn-text">Ich bin neu hier</span>
-            </button>
-            <button className="btn nav-button next-button relative-button fade-in arya-animation animation3" onClick={this.props.navLogin}>
-              <span className="btn-text">Anmelden </span>
-            </button>
-          </div>
+
         </div>
 
       </div>
